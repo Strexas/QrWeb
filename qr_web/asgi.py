769ac350a00d166e3d008615f42b06ca771637b8
Dainius <kirsnauskas1@gmail.com>
2024-03-08 21:32:16 +0200

    renamed module, suppresed warning

--- qr_web/asgi.py
+++ qr_web/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for QrWeb project.
+ASGI config for qr_web project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'QrWeb.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'qr_web.settings')
 
 application = get_asgi_application()
