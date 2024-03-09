b821c9abf8f2e4b2175cc6953ee0f7fe3951adfa
Anna <anna.levchenko.2004@gmail.com>
2024-03-09 20:52:43 +0200

    update settings.py

--- qr_web/settings.py
+++ qr_web/settings.py
@@ -1,4 +1,3 @@
-from typing import List
 """
 Django settings for qr_web project.
 
@@ -10,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+from typing import List
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
