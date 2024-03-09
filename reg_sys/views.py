18f64443949ceae857f626700b9cda519702954b
Akaud <akadlevchenko@gmail.com>
2024-03-09 16:45:27 +0200

    fixed bug that would not alow user to login after registration

--- reg_sys/views.py
+++ reg_sys/views.py
@@ -17,7 +17,7 @@ def register(request):
         form = CreateUserForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('user-login')
+            return redirect('login')
     context = {'form': form}
     return render(request, 'reg_sys/register.html', context=context)
 
