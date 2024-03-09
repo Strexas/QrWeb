9a3b3560f185571c0961b745b3941d461aeddef1
Anna <anna.levchenko.2004@gmail.com>
2024-03-09 20:47:00 +0200

    settings.py->1- from typing import List,29-># type: List[str]

--- qr_web/wsgi.py
+++ qr_web/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for QrWeb project.
+WSGI config for qr_web project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'QrWeb.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'qr_web.settings')
 
 application = get_wsgi_application()
