c3ac539a474f437fd5468d7bd3a15241038e1338
Akaud <akadlevchenko@gmail.com>
2024-03-12 18:17:36 +0200

    views.py->docs strings

--- reg_sys/views.py
+++ reg_sys/views.py
@@ -1,18 +1,20 @@
+"""views.py"""
 from django.shortcuts import render, redirect
-from .forms import UserRegisterForm, LoginForm
 from django.contrib.auth import authenticate
 from django.contrib import messages, auth
 from django.contrib.auth.decorators import login_required
+from .forms import UserRegisterForm, LoginForm
 # Create your views here.
 
 
-# Homepage
 def home(request):
+    """Homepage"""
     return render(request, 'reg_sys/startpage.html')
 
 
 # --Register a user
 def register(request):
+    """Registration process"""
     if request.method == 'POST':  # Checking if we are sending data to database
         form = UserRegisterForm(request.POST)
         if form.is_valid():
@@ -27,6 +29,7 @@ def register(request):
 
 # - Login a user
 def user_login(request):
+    """Login process"""
     form = LoginForm()
     if request.method == 'POST':
         form = LoginForm(request, data=request.POST)
@@ -44,6 +47,7 @@ def user_login(request):
 
 # - User logout
 def user_logout(request):
+    """Logout process"""
     auth.logout(request)
     return redirect('login')
 
@@ -51,4 +55,5 @@ def user_logout(request):
 # - Profile page
 @login_required(login_url='login')
 def user_profile(request):
+    """Registration process"""
     return render(request, 'reg_sys/profile.html')
