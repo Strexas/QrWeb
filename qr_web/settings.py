45c04d6a6fc7ddc0fcb0229c4c3992c73431f81b
Akaud <akadlevchenko@gmail.com>
2024-03-17 14:37:20 +0200

    Updated ALLOWED_HOSTS in settings.py

--- qr_web/settings.py
+++ qr_web/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-69mez*=oxw)h+a60&8-15kxz&xh%dh@&6k^mt&zre^g507aqt_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = list[str]
+ALLOWED_HOSTS = [str]
 
 
 # Application definition
