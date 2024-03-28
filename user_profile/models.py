c7b347b1eff49a75a84780a1479eb9392429468b
RustamHabibovv <rustamhabibov2004@gmail.com>
2024-03-28 16:18:58 +0200

    Solving mypy problems

--- user_profile/models.py
+++ user_profile/models.py
@@ -9,8 +9,8 @@ from PIL import Image
 # Create your models here.
 
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField(default='default.png',upload_to='profile_pics')
+    user : models.OneToOneField = models.OneToOneField(User, on_delete=models.CASCADE)
+    image : models.ImageField = models.ImageField(default='default.png',upload_to='profile_pics')
 
     def __str__(self):
         return f'{self.user.username} Profile'
