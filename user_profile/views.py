a4b8a9f75bcc85bc24fe3405e1f48d0f02ae75ac
Akaud <akadlevchenko@gmail.com>
2024-04-18 22:39:33 +0300

    grouping imports in user_profile/views.py

--- user_profile/views.py
+++ user_profile/views.py
@@ -2,8 +2,9 @@
 from django.contrib import messages
 from django.contrib.auth import update_session_auth_hash
 from django.contrib.auth.forms import PasswordChangeForm
-from django.shortcuts import render, redirect,get_object_or_404
 from django.contrib.auth.decorators import login_required
+
+from django.shortcuts import render, redirect, get_object_or_404
 
 from constructor.forms import PageForm
 from .forms import UpdateImageForm
