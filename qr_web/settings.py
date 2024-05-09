08adb4ed8507f75e19beae55c9cc6e93b660d1ff
RustamHabibovv <rustamhabibov2004@gmail.com>
2024-05-09 22:17:35 +0300

    Solving pylint E5110

--- qr_web/settings.py
+++ qr_web/settings.py
@@ -14,7 +14,6 @@ import os.path
 from pathlib import Path
 import environ  # type: ignore[import-untyped]
 
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
