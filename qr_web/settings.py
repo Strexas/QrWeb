fe32c4827c1877197b5e67542e6e61a236cd9741
Dainius Kirsnauskas <kirsnauskas@gmail.com>
2024-05-09 21:14:00 +0300

    fix of mime

--- qr_web/settings.py
+++ qr_web/settings.py
@@ -13,6 +13,11 @@ import os
 import os.path
 from pathlib import Path
 
+import mimetypes
+
+
+mimetypes.add_type("text/css", ".css", True)
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
