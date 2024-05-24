36d29632854ed90afcb9df9e89ccc12a0cecefff
Dainius Kirsnauskas <kirsnauskas@gmail.com>
2024-05-24 10:57:55 +0300

    too long lines remove

--- qr_web/settings.py
+++ qr_web/settings.py
@@ -35,9 +35,11 @@ DEBUG = 'RENDER' not in os.environ
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 # This setting informs Django of the URI path from which your static files will be served to users
-# Here, they well be accessible at your-domain.onrender.com/static/... or yourcustomdomain.com/static/...
+# Here, they well be accessible at your-domain.onrender.com/static/...
+# or yourcustomdomain.com/static/...
 STATIC_URL = '/static/'
-# This production code might break development mode, so we check whether we're in DEBUG mode
+# This production code might break development mode,
+# so we check whether we're in DEBUG mode
 if not DEBUG:    # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
