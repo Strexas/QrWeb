fa857c483b9eab5c163bc2c201043338e7c4ed4f
Dainius Kirsnauskas <kirsnauskas@gmail.com>
2024-05-25 13:49:53 +0300

    storage fix

--- qr_web/settings.py
+++ qr_web/settings.py
@@ -59,7 +59,7 @@ if RENDER_EXTERNAL_HOSTNAME:
 GS_CREDENTIALS = service_account.Credentials.from_service_account_file(
     os.path.join(BASE_DIR, 'credential.json')
 )
-DEFAULT_FILE_STORAGE = 'storages.backends.gcloud.GoogleCloudStorage'
+DEFAULT_FILE_STORAGE = 'django.cores.files.storage.FileSystemStorage'
 GS_BUCKET_NAME = 'qr_web_image_bucket'
 GS_DEFAULT_ACL = 'publicRead'
 
