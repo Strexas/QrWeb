bc63b42c9c5228ab588994180591e0a564cc44b0
RustamHabibovv <rustamhabibov2004@gmail.com>
2024-05-26 11:10:10 +0300

    Static code analysis file updates

--- static_code_analysis.py
+++ static_code_analysis.py
@@ -1,15 +1,14 @@
-"""Runs mypy and pylint"""
-
 import subprocess
 
 # MyPy
 subprocess.run("mypy --install-types --non-interactive".split(), check=True)
-subprocess.run("mypy ./".split(), check=False)
+subprocess.run("mypy ./ --exclude venv".split(), check=False)
 
 # PyLint
-subprocess.run("pylint -j 0\
-              --load-plugins=pylint_django \
-              --max-parents=14 \
-              --django-settings-module=qr_web.settings "
+subprocess.run("pylint -j 0 "
+               "--load-plugins=pylint_django "
+               "--max-parents=14 "
+               "--django-settings-module=qr_web.settings "
                "--recursive=y "
-               "--ignore=.venv ./".split(), check=False)
+               "--ignore=venv "
+               "./".split(), check=False)
